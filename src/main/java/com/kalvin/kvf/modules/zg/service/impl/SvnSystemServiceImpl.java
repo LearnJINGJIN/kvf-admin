5878436fd6fb8514197cf158052dad03ee04ded9
jingjin <1483299608@qq.com>
2020-12-15 16:58:08 +0800

    修改impl目录

--- src/main/java/com/kalvin/kvf/modules/zg/service/impl/SvnSystemServiceImpl.java
+++ src/main/java/com/kalvin/kvf/modules/zg/service/impl/SvnSystemServiceImpl.java
@@ -1,7 +1,8 @@
-package com.kalvin.kvf.modules.zg.service;
+package com.kalvin.kvf.modules.zg.service.impl;
 
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.kalvin.kvf.modules.zg.service.SvnSystemService;
 import org.springframework.stereotype.Service;
 import com.kalvin.kvf.modules.zg.entity.SvnSystem;
 import com.kalvin.kvf.modules.zg.mapper.SvnSystemMapper;
